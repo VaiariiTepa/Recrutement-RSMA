bc7d648485b26abf70db2064cc3bd046aad3d976
VaiariiTepa <tepavaiarii@gmail.com>
2020-02-15 22:29:19 -1000

    mcr create

---
+++ app/Http/Controllers/ImportexcelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Importexcel;
+use Illuminate\Http\Request;
+
+class ImportexcelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Importexcel  $importexcel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Importexcel $importexcel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Importexcel  $importexcel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Importexcel $importexcel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Importexcel  $importexcel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Importexcel $importexcel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Importexcel  $importexcel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Importexcel $importexcel)
+    {
+        //
+    }
+}
